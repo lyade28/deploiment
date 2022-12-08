f3641119d31e4e69d801e811f8073d66faa2ea6d
Libasse Yade <lyade@gainde2000.sn>
2022-12-08 15:31:42 +0000

    all

--- api/settings.py
+++ api/settings.py
@@ -27,15 +27,9 @@ SECRET_KEY = 'django-insecure-^fftm$6rsjiatu==4vhn8ji(olwwt0h*q2)%_!%@1&t+9xp&j4
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = [
-    '*'
-],
-ALLOWED_HOSTS = [
-    '*',
-],
-CORS_ORIGIN_WHITELIST = [
-    '*',
-]
+CSRF_TRUSTED_ORIGINS = ['https://web-production-593d.up.railway.app/']
+
+
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
